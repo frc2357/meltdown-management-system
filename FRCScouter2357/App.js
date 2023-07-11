ff3dddb7c7f2fbedeccf292ffec896d687783db2
NolanCampbell2357 <nolanc1223@gmail.com>
2023-07-11 18:34:25 -0500

    Remove storybook

--- FRCScouter2357/App.js
+++ FRCScouter2357/App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Constants from 'expo-constants';
 
 function App() {
   return (
@@ -20,13 +19,4 @@ const styles = StyleSheet.create({
   },
 });
 
-
-// Default to rendering your app
-let AppEntryPoint = App;
-
-// Render Storybook if storybookEnabled is true
-if (Constants.expoConfig.extra.storybookEnabled === 'true') {
-  AppEntryPoint = require('./.storybook').default;
-}
-
-export default AppEntryPoint;
+export default App;
