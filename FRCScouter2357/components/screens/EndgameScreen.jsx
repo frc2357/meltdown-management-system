8b4f75a012de6ca2015abdc41c82d6d9e24d08cb
NolanCampbell2357 <nolanc1223@gmail.com>
2023-08-08 18:25:31 -0500

    Add navigation object

--- FRCScouter2357/components/screens/EndgameScreen.jsx
+++ FRCScouter2357/components/screens/EndgameScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function EndgameScreen() {
+export default function EndgameScreen({navigation}) {
     return (
         <></>
     )
--- FRCScouter2357/components/screens/PrematchScreen.jsx
+++ FRCScouter2357/components/screens/PrematchScreen.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function PrematchScreen() {
+export default function PrematchScreen({navigation}) {
     return (
         <></>
     )
