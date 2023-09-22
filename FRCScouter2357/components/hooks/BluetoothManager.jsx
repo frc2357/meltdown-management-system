8641a8851fe00502f10fe907b1acc7331e164c3d
NolanCampbell2357 <nolanc1223@gmail.com>
2023-09-21 19:46:01 -0500

    Basic bluetooth hook

---
+++ FRCScouter2357/components/hooks/BluetoothManager.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState, useRef } from 'react';
+import RNBluetoothClassic, { BluetoothDevice } from 'react-native-bluetooth-classic';
+
+export default function BluetoothManager() {
+  const [device, setDevice] = useState();
+  const [currentMatch, setCurrentMatch] = useState();
+  const [assignment, setAssignment] = useState();
+  const bluetoothSub = useRef();
+
+  useEffect(() => {
+    bluetoothSub.current = RNBluetoothClassic.onDeviceConnected((event) => {
+      setDevice(event.device);
+
+      if (device.isConnected()) {
+        device.onDataReceived((event) => {
+          const data = event.data;
+
+          switch (data.type) {
+            case 'match':
+              setCurrentMatch(data.info);
+              break;
+            case 'assignment':
+              setAssignment(assignment.info);
+              break;
+          }
+        });
+      }
+    });
+
+    return () => {
+      bluetoothSub.current.remove();
+    };
+  }, []);
+
+  const getNextMatch = () => {
+    return currentMatch;
+  };
+
+  const getAssignment = () => {
+    return assignment;
+  };
+
+  const upload = (matchLog) => {
+    JSON.stringify(matchLog);
+    device.write(matchLog, "ascii");
+  };
+
+  return {
+    isInit: device !== null,
+    getNextMatch,
+    getAssignment,
+    upload,
+  };
+}
