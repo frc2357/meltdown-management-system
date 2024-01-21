d6dbdb6188dbfe3941af6e6b994a3cc1643142fc
NolanCampbell2357 <nolanc1223@gmail.com>
2024-01-21 14:02:50 -0600

    Init react native windows

---
+++ ScoutingCenter/windows/ScoutingCenter/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ScoutingCenter::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ScoutingCenter::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
