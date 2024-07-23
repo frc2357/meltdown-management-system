206e70d76dde8deb720f291613a33ef0280704fb
Nolan Campbell <nolanc1223@gmail.com>
2024-07-23 18:41:57 -0500

    start capacitor

---
+++ scoutingmanagementsystem/sentinel/android/app/src/main/java/ionic/sentinel/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.sentinel;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ scoutingmanagementsystem/sentinel/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.sentinel',
+  appName: 'sentinel',
+  webDir: 'dist'
+};
+
+export default config;
