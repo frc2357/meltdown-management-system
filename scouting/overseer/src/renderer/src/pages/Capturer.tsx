ff72bdd301ff259fa5ba8cb02bc6ac65d1857515
Nolan Campbell <nolanc1223@gmail.com>
2025-07-07 19:31:54 -0500

    Add multi-select camera

--- scouting/overseer/src/renderer/src/pages/Capturer.tsx
+++ scouting/overseer/src/renderer/src/pages/Capturer.tsx
@@ -7,15 +7,20 @@ import {
   DialogContentText,
   DialogTitle,
   LinearProgress,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
 } from '@mui/material';
 import { QrScanner } from '@yudiel/react-qr-scanner';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export function Capturer(): ReactElement {
   const [isProcessing, setProcessing] = useState<boolean>(false);
   const [isAlertOpen, setAlertOpen] = useState<boolean>(false);
   const [goodScan, setGoodScan] = useState<boolean>(false);
+  const [camera, setCamera] = useState<string>('default');
+  const [allCameras, setAllCameras] = useState<MediaDeviceInfo[]>([]);
 
   const handleScan: (data: string) => void = (data: string): void => {
     setProcessing(true);
@@ -35,9 +40,34 @@ export function Capturer(): ReactElement {
     setAlertOpen(false);
   };
 
+  const handleCameraSelect: (event: SelectChangeEvent) => void = (event: SelectChangeEvent) => {
+    setCamera(event.target.value as string);
+  };
+
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then((devices) => {
+      const cameras = devices.filter((device) => {
+        return device.kind === 'videoinput';
+      });
+      setAllCameras(cameras);
+    });
+  }, []);
+
+  const memoizedQrScanner = useMemo(() => {
+    return (
+      <QrScanner
+        stopDecoding={isAlertOpen && !isProcessing} // Prevent decoding when alert is open and not processing
+        onDecode={handleScan}
+        onError={handleError}
+        deviceId={camera === 'default' ? undefined : camera}
+        audio={false}
+      />
+    );
+  }, [isAlertOpen, isProcessing, camera]);
+
   return (
     <Box sx={{ marginLeft: '16px' }}>
-      <QrScanner stopDecoding={isAlertOpen} onDecode={handleScan} onError={handleError} />
+      {memoizedQrScanner}
       <Button
         sx={{ marginTop: '10px' }}
         variant="contained"
@@ -46,6 +76,16 @@ export function Capturer(): ReactElement {
       >
         Distributor
       </Button>
+      <Select value={camera} label="Cameras" onChange={handleCameraSelect}>
+        <MenuItem value={'default'}>Default</MenuItem>
+        {allCameras.map((camera) => {
+          return (
+            <MenuItem key={camera.deviceId} value={camera.deviceId}>
+              {camera.label}
+            </MenuItem>
+          );
+        })}
+      </Select>
       <Dialog
         open={isAlertOpen}
         onClose={handleClose}
